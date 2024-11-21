f5bdd379a0579b5238e6b7cad2e38d0235b2bc1f
Alasdair PB <39963762+Alasdair-PB@users.noreply.github.com>
2024-11-21 10:54:52 +0000

    Sphere physics fix & ai state machine implemention

--- CSC8503/Main.cpp
+++ CSC8503/Main.cpp
@@ -50,35 +50,6 @@ hide or show the
 
 */
 
-void TestStateMachine() {
-	StateMachine* testMachine = new StateMachine();
-	int data = 0;
-
-	State* A = new State([&](float dt) -> void {
-		std::cout << "I’m in state A!\n";
-		data++;
-		});
-
-	State* B = new State([&](float dt) -> void {
-		std::cout << "I’m in state B!\n";
-		data--;
-		});
-
-	StateTransition* stateAB = new StateTransition(A, B, [&](void)-> bool {return data > 10;});
-	StateTransition* stateBA = new StateTransition(B, A, [&](void)-> bool {return data < 0;});
-
-	testMachine->AddState(A);
-	testMachine->AddState(B);
-	testMachine->AddTransition(stateAB);
-	testMachine->AddTransition(stateBA);
-
-	for (int i = 0; i < 100; ++i) {
-		testMachine->Update(1.0f);
-	}
-
-
-}
-
 
 int main() {
 	WindowInitialisation initInfo;
--- NCLCoreClasses/Vector.h
+++ NCLCoreClasses/Vector.h
@@ -295,7 +295,7 @@ namespace NCL::Maths {
         template <typename T, uint32_t n>
         constexpr T		GetMinElement(const VectorTemplate<T, n>& a) {
             float v = a.array[0];
-            for (int i = 1; i < n; ++i) {
+            for (int i = 0; i < n; ++i) {
                 v = std::min(v, a.array[i]);
             }
             return v;
@@ -304,7 +304,7 @@ namespace NCL::Maths {
         template <typename T, uint32_t n>
         constexpr T		GetMaxElement(const VectorTemplate<T, n>& a) {
             float v = a.array[0];
-            for (int i = 1; i < n; ++i) {
+            for (int i = 0; i < n; ++i) {
                 v = std::max(v, a.array[i]);
             }
             return v;
@@ -313,7 +313,7 @@ namespace NCL::Maths {
         template <typename T, uint32_t n>
         constexpr T		GetAbsMaxElement(const VectorTemplate<T, n>& a) {
             float v = std::abs(a.array[0]);
-            for (int i = 1; i < n; ++i) {
+            for (int i = 0; i < n; ++i) {
                 v = std::max(v, std::abs(a.array[i]));
             }
             return v;
@@ -322,7 +322,7 @@ namespace NCL::Maths {
         template <typename T, uint32_t n>
         constexpr VectorTemplate<T, n>		Clamp(VectorTemplate<T, n>& input, VectorTemplate<T, n>& mins, VectorTemplate<T, n>& maxs) {
             VectorTemplate<T, n> output;
-            for (int i = 1; i < n; ++i) {
+            for (int i = 0; i < n; ++i) {
                 output.array[i] = std::clamp(input.array[i], mins.array[i], maxs.array[i]);
             }
             return output;
